03c04fb9761d77c5c528336a9bc467d208d781bc
hiroshi nagao <hi_1205@hotmail.com>
2024-06-19 10:29:08 +0900

    add generate password function

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
-function generatePassword(){
-  
+function generatePassword(length, includeLowerCase, includeUpperCase, includeNumbers, includeSymbols){
+    return '';
 }
 
 
@@ -8,3 +8,9 @@ const includeLowerCase = true;
 const includeUpperCase = true;
 const includeNumbers = true;
 const includeSymbols = true;
+
+const password = generatePassword(passwordLength,
+                                 includeLowerCase,
+                                 includeUpperCase,
+                                 includeNumbers,
+                                 includeSymbols )
