6f2b9d4e965817a02ad049c148e4e653dbfd78a0
hiroshi nagao <hi_1205@hotmail.com>
2024-06-19 10:22:10 +0900

    initial commit

---
+++ index.js
@@ -0,0 +1,10 @@
+function generatePassword(){
+  
+}
+
+
+const passwordLength = 12;
+const includeLowerCase = true;
+const includeUpperCase = true;
+const includeNumbers = true;
+const includeSymbols = true;
